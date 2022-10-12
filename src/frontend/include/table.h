ca4389598d94739011673a3372ec2a1c3fb744a2
LuCC-que <chen1968493689@gmail.com>
2022-10-12 18:10:21 -0400

    get some food, then back to work

--- src/frontend/include/table.h
+++ src/frontend/include/table.h
@@ -4,6 +4,8 @@
 
 #define COLUMN_USERNAME_SIZE 32
 #define COLUMN_EMAIL_SIZE 255
+
+// joey shortcut
 #define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
 
 typedef struct {
@@ -16,6 +18,8 @@ typedef struct {
 const uint32_t ID_SIZE = size_of_attribute(Row, id);
 const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
 const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+
+// &ID_OFFSET &USERNAME_OFFSET &EMAIL_OFFSET
 const uint32_t ID_OFFSET = 0;
 const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
 const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
